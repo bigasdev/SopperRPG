f98b7ba951900f2e69afdb4756a6595218545600
bigassopper <61640594+bigassopper@users.noreply.github.com>
2020-07-25 04:53:08 -0300

    Movement

--- Assets/Scripts/Controller/Controller.cs
+++ Assets/Scripts/Controller/Controller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent (typeof (BoxCollider2D))]
 public class Controller : MonoBehaviour
 {
+    public LayerMask collisionMask;
+    
     const float skinWidth = .15f;
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
@@ -18,16 +20,35 @@ public class Controller : MonoBehaviour
     private void Start()
     {
         collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
     }
 
-    private void Update()
+    public void Move(Vector3 velocity)
     {
         UpdateRaycastOrigins();
-        CalculateRaySpacing();
+
+        VerticalCollisions(ref velocity);
+
+        transform.Translate(velocity);
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLenght = Mathf.Abs(velocity.y) + skinWidth;
 
         for (int i = 0; i < verticalRayCount; i++)
         {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLenght, collisionMask);
             Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+
+            if(hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLenght = hit.distance;
+            }
         }
     }
 
--- Assets/Scripts/Controller/Player.cs
+++ Assets/Scripts/Controller/Player.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 [RequireComponent (typeof (Controller))]
 public class Player : MonoBehaviour
 {
+    float gravity = 50;
+    int moveSpd = 5;
+    Vector3 velocity;
+    
     Controller controller;
     void Start()
     {
         controller = GetComponent<Controller>(); 
+    }
+
+    private void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+
+        velocity.x = input.x * moveSpd;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
 }
