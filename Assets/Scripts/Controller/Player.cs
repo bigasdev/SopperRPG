be5daa32dc0f6d8e20b25e6085993f165eed991f
bigassopper <61640594+bigassopper@users.noreply.github.com>
2020-07-25 03:41:27 -0300

    Infernal Code

---
+++ Assets/Scripts/Controller/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller))]
+public class Player : MonoBehaviour
+{
+    Controller controller;
+    void Start()
+    {
+        controller = GetComponent<Controller>(); 
+    }
+
+}
